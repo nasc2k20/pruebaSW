f6b4c483ddd790f2df9bcc019c11bbb0db02a190
Nasc2k12 <nasc2k12@fsapadua.com>
2023-07-07 16:16:24 -0600

    Actualizando

--- pruebaSW/conexion.vb
+++ pruebaSW/conexion.vb
@@ -3,10 +3,12 @@ Module conexion
 
     'Public cnn As String = "Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;"
 
-    Dim dbServer As String = "localhost\SQLEXPRESS"
+    Dim dbServer As String = "DB-TECH"
     Dim dbName As String = "pruebaSW"
+    Dim dbUser As String = "sa"
+    Dim dbPassword As String = "Db$2023"
 
-    'Public cnn As New SqlConnection("Initial Catalog=Northwind;Data Source=localhost;Integrated Security=SSPI;")
-    Public cnn As New SqlConnection("Server=" & dbServer & ";Database=" & dbName & ";Trusted_Connection=True;")
+    Public cnn As New SqlConnection("Data Source=" & dbServer & ";Initial Catalog=" & dbName & ";
+                                        User ID=" & dbUser & "; password=" & dbPassword & ";")
 
 End Module
